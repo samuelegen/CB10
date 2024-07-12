3292db17a430ef2b4f19e9af64ce6410d3c03e58
samuelegen <samuelegennaro@gmail.com>
2024-07-12 15:23:09 +0200

    fix: image

--- 12-07-2024 react tailwind/src/App.jsx
+++ 12-07-2024 react tailwind/src/App.jsx
@@ -1,11 +1,11 @@
+import ethereum from './assets/image/images/image-equilibrium.jpg'
 function App() {
-
   return (
     <>
     <a href="#" className="block rounded-lg p-4 w-80 h-2/4 bg-[#15253F] shadow-2xl">
   <img
-    alt=""
-    src="https://images.unsplash.com/photo-1613545325278-f24b0cae1224?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+    alt="logo"
+    src={ethereum}
     className="h-56 w-full rounded-md object-cover"
   />
 
