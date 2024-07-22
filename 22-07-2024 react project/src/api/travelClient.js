5b32e9e92a9011c9116cf32b7c08e80cd24842af
samuelegen <samuelegennaro@gmail.com>
2024-07-22 16:36:06 +0200

    feat: add new project

---
+++ 22-07-2024 react project/src/api/travelClient.js
@@ -0,0 +1,39 @@
+// export const getBookList = () => {
+// 	return new Promise((resolve) => {
+// 		setTimeout(() => {
+// 			resolve(() => {
+// 				return [
+// 					{
+// 						id: "1",
+// 						title: "Paperino alla scoperta del mondo",
+// 						genre: "fantascienza",
+// 						author: "Pippo e pluto",
+// 					},
+// 				];
+// 			});
+// 		}, 2000);
+// 	});
+// };
+
+// export const getraveList = async () => {
+// 	try {
+// 		const res = await fetch(
+// 			"https://trailapi-trailapi.p.rapidapi.com/activity/?lat=34.1&limit=25&lon=-105.2&q-city_cont=Denver&q-country_cont=Australia&q-state_cont=California&radius=25&q-activities_activity_type_name_eq=hiking"
+// 		);
+// 		return res.json();
+// 	} catch (error) {
+// 		throw new Error("Error:", error);
+// 	}
+// };
+
+
+const url = 'https://fakestoreapi.com/products';
+export const getraveList = async () => {
+try {
+	const response = await fetch(url);
+	const result = await response.json();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
+};
---
+++ 22-07-2024 react project/src/data/labels.js
@@ -0,0 +1,7 @@
+export const labels = {
+	ProductsTitle: "Title",
+	ProductsCategory: "Category",
+	ProductsPrice: "Price",
+    ProductsRate: "Rating",
+	ProductsList: "Products list"
+}
