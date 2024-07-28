5f7e3f9092be838668b7e06330b469cfdd40ffde
samuelegen <samuelegennaro@gmail.com>
2024-07-28 15:16:20 +0200

    fix: input filter

--- 22-07-2024 react project/src/App.jsx
+++ 22-07-2024 react project/src/App.jsx
@@ -91,9 +91,12 @@ function App() {
 											<td className="whitespace-nowrap px-4 py-2">
 												<Link
 													to={`/product/${product.id}`}
-													className="inline-block rounded bg-indigo-600 px-4 py-2 text-xs font-medium text-white hover:bg-indigo-700"
+													className="inline-block rounded bg-indigo-600 px-4 py-2 text-xs font-medium text-white hover:bg-indigo-700 m-2"
 												>
 													View
+												</Link>
+												<Link className="inline-block rounded bg-red-600 px-4 py-2 text-xs font-medium text-white">
+													Delete
 												</Link>
 											</td>
 										</tr>
--- 22-07-2024 react project/src/api/ProductsClient.js
+++ 22-07-2024 react project/src/api/ProductsClient.js
@@ -15,17 +15,6 @@
 // 	});
 // };
 
-// export const getraveList = async () => {
-// 	try {
-// 		const res = await fetch(
-// 			"https://trailapi-trailapi.p.rapidapi.com/activity/?lat=34.1&limit=25&lon=-105.2&q-city_cont=Denver&q-country_cont=Australia&q-state_cont=California&radius=25&q-activities_activity_type_name_eq=hiking"
-// 		);
-// 		return res.json();
-// 	} catch (error) {
-// 		throw new Error("Error:", error);
-// 	}
-// };
-
 const url = "https://fakestoreapi.com/products";
 
 export const getProductsList = async (id) => {
